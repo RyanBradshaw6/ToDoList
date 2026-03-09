d3460bb7124688c9eea3e903532bc3660e076638
Ryan Bradshaw <bradshr@bc.edu>
2026-03-09 11:40:59 -0400

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Ryan Bradshaw on 3/9/26.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
